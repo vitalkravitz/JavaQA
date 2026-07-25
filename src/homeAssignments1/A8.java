3dcb8cc834d78bd275be2de9510c5b2a409df122
vitalkravitz <vitalkravitz@gmail.comj>
2026-07-25 14:00:13 +0300

    QA

--- src/homeAssignments1/A8.java
+++ src/homeAssignments1/A8.java
@@ -6,6 +6,12 @@ public class A8 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a word: ");
-        int word = in.nextInt();
+        String word = in.nextLine();
+
+        //print out the length of the word, the first and last letters, and does it equals: "java" (not case sensitive)
+        System.out.println("Length: " + word.length());
+        System.out.println("First letter: " + word.charAt(0));
+        System.out.println("Last letter: " + word.charAt(word.length() - 1));
+        System.out.println("Equals 'java': " + word.equalsIgnoreCase("java"));
     }
 }
